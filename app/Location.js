4a6c77d92fce30ccdce70f4799b3b32ca849f3ac
khadizajarin <khadiza131310@gmail.com>
2024-04-05 15:24:48 +0600

    location and REST api added

--- app/Location.js
+++ app/Location.js
@@ -1,114 +1,172 @@
-import { StyleSheet, Text, View, Dimensions, Button } from 'react-native'
-import MapView, { Marker } from 'react-native-maps';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity,  ActivityIndicator } from 'react-native'; // Add ActivityIndicator
+import MapView, { Marker, Polyline } from 'react-native-maps';
 import * as Location from 'expo-location';
-import { ScrollView } from 'react-native';
-
 
 const LocationScreen = () => {
-    const [map, setMap] = useState({
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-    });
+  const [map, setMap] = useState({
+    latitude: 23.6933,
+    longitude: 90.3818,
+    latitudeDelta: 0.5,
+    longitudeDelta: 0.5,
+  });
 
-    const useLocation = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied');
-        }
-        let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
-        setMap({
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-        });
+  const [userLocation, setUserLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true); 
+  const mapViewRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        setLoading(false); 
+      }
+
+      try {
+        let location = await Location.getCurrentPositionAsync({});
+        setUserLocation(location.coords);
+
         fetchWeather(location.coords.latitude, location.coords.longitude);
-    }
-
-    useEffect(() => {
-        useLocation();
-    }, []);
-
-    const scrollToUserLocation = async () => {
-        await useLocation();
-        mapViewRef.current.animateToRegion({
-            latitude: map.latitude,
-            longitude: map.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-        }, 1000);
-    }
-
-    const mapViewRef = React.createRef();
-
-
-
-
-    const [weather, setWeather] = useState(null);
-    const API_KEY = '68b6d6acd8fbe078d54eecc8971d7b40'
   
-    const fetchWeather = async (latitude, longitude) => {
-        try {
-            const response = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
-            );
-            const data = await response.json();
-            console.log(data);
-            setWeather(data);
-        } catch (error) {
-            console.error('Error fetching weather:', error);
-        }
-    };
-    
-    
+        setMap({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.5,
+          longitudeDelta: 0.5,
+        });
+      } catch (error) {
+        console.error('Error fetching location:', error);
+        setLoading(false); 
+      }
+    })();
+  }, []);
 
-    return (
-        <View style={styles.container}>
-            <View style={{ borderBottomWidth: 1, borderBottomColor: '#AB8C56' }}>
-                
-            </View>
-            <Text style={{ fontFamily: "serif", fontSize: 40, fontWeight: 'bold',  padding:10, paddingBottom:10, color: '#3A3D42', borderBottomWidth: 1, borderBottomColor: '#AB8C56'}}>Let's see where we are located!</Text>
+  const fetchWeather = async (latitude, longitude) => {
+    try {
+      const API_KEY = '68b6d6acd8fbe078d54eecc8971d7b40';
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      );
+      const data = await response.json();
+      setWeather(data);
+      setLoading(false); 
+    } catch (error) {
+      console.error('Error fetching weather:', error);
+      setLoading(false); 
+    }
+  };
+
+  const renderPolyline = () => {
+    if (userLocation) {
+      return (
+        <Polyline
+          coordinates={[
+            { latitude: userLocation.latitude, longitude: userLocation.longitude },
+            { latitude: 23.6933, longitude: 90.3818 },
+          ]}
+          strokeColor="#FF0000"
+          strokeWidth={3}
+        />
+      );
+    }
+    return null;
+  };
+
+  const scrollToUserLocation = async () => {
+    mapViewRef.current.animateToRegion({
+      latitude: map.latitude,
+      longitude: map.longitude,
+      latitudeDelta: 0.5,
+      longitudeDelta: 0.5,
+    });
+  };
+
+  return (
+
+      <View style={styles.container}>
+        <Text style={styles.header}>Let's see where we are located!</Text>
+        {loading ? ( 
+          <ActivityIndicator size="large" color="#3A3D42" />
+        ) : (
+          <>
             <MapView
-                ref={mapViewRef}
-                style={styles.map}
-                region={map}>
-                <Marker coordinate={map} title='Marker' />
+              ref={mapViewRef}
+              style={styles.map}
+              region={map}
+              showsUserLocation={true}
+              followsUserLocation={true}
+            >
+              <Marker coordinate={{ latitude: 23.6933, longitude: 90.3818 }} title="EvePlano" />
+              {renderPolyline()}
             </MapView>
             {weather && (
-            <View style={styles.weatherContainer}>
+              <View style={styles.weatherContainer}>
+                <Text style={styles.weatherText}>Get to know abouthe weather {'\n'}of your current location:</Text>
                 <Text style={styles.weatherText}>Weather: {weather.weather[0].main}</Text>
                 <Text style={styles.weatherText}>Description: {weather.weather[0].description}</Text>
                 <Text style={styles.weatherText}>Temperature: {weather.main.temp}°C</Text>
-            </View>
+                <Text style={styles.weatherText}>Humidity: {weather.main.humidity}%</Text>
+                <Text style={styles.weatherText}>Wind Speed: {weather.wind.speed} m/s</Text>
+                <Text style={styles.weatherText}>Pressure: {weather.main.pressure} hPa</Text>
+                <Text style={styles.weatherText}>Visibility: {weather.visibility / 1000} km</Text>
+              </View>
             )}
-            <Button title='Get Location' onPress={scrollToUserLocation}></Button>
-        </View>
-    );
-}
-
-export default LocationScreen;
+            <TouchableOpacity style={styles.button} onPress={scrollToUserLocation}>
+              <Text style={styles.buttonText}>Get Your Location</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    map: {
-        width: '100%',
-        height: '50%',
-    },
-    weatherContainer: {
-        position: 'absolute',
-        top: 30,
-        left: 20,
-        backgroundColor: '#00bfff',
-        padding: 20,
-        borderRadius: 10,
-      },
-      weatherText: {
-        fontSize: 20,
-        color: '#ffffff',
-      },
+ container:{
+    display:'flex',
+    alignItems:'center',
+    justifyContent: 'center'
+ },
+  header: {
+    fontFamily: 'serif',
+    fontSize: 20,
+    marginTop: 10,
+    fontWeight: 'bold',
+    color: '#3A3D42',
+    marginBottom: 10,
+  },
+  map: {
+    width: '100%',
+    height: '80%',
+  },
+  weatherContainer: {
+    position: 'absolute',
+    bottom: 40,
+    right: 10,
+    backgroundColor: '#3A3D42',
+    padding: 10,
+    borderRadius: 5,
+  },
+  weatherText: {
+    fontSize: 12,
+    color: '#AB8C56',
+    marginBottom:2,
+  },
+  button: {
+    marginTop:10,
+    backgroundColor: '#3A3D42',
+    padding: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#AB8C56',
+    fontSize: 16,
+    padding:5,
+    fontWeight: 'bold',
+  },
 });
+
+export default LocationScreen;
